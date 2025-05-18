be62402d3cfaae5fd14e77a5ebe6d9df1f7e803a
Marinelic <75620592+Marinelic@users.noreply.github.com>
2025-05-18 18:51:44 +0200

    Fixing light and dark mode

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 
 export const Home = () => {
-    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden" id="light">
 
         {/* Theme Toggle */}
             <ThemeToggle />
