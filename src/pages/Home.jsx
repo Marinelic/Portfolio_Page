aa6d5b2631df73a9599183a0de8d5e268e064391
Marinelic <75620592+Marinelic@users.noreply.github.com>
2025-06-04 20:44:21 +0200

    Adding HeroSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,24 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
-/* import { HeroSection } from "../components/HeroSection"; */
+import { HeroSection } from "../components/HeroSection";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden" id="light">
 
         {/* Theme Toggle */}
             <ThemeToggle />
+
         {/* Background Effects */}
             <StarBackground />
+
         {/* Navbar */}
             <Navbar />
+
         {/* Main Content */}
-       {/*  <main>
-            <HeroSection />
-        </main> */}
+            <main>
+                <HeroSection />
+            </main> 
         {/* Footer */}
 
 
